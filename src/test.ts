ceda8a63ae382e6712fb6158aaca66701f606ba8
Limboyanking <yanking3516@gmail.com>
2023-04-04 16:08:26 -0400

    Conduct system tests and validate the application functionality

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,10 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
+/*
+  Customize into test
+  
+*/
+
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import {
